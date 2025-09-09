bd15b2082f527f80c8700cab20a220c3c4e9a17a
Nanyangu-ommy <onanseta@byupathway.edu>
2025-09-09 07:13:22 -1000

    Home page

---
+++ scripts/getdates.js
@@ -0,0 +1,13 @@
+// getdates.js
+
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+// Insert current year into the first footer paragraph
+document.querySelector("footer p").innerHTML = `&copy; ${currentYear} 🌸 Omega Ommy Nanseta 🌸 Malawi`;
+
+// Get last modified date
+const lastModified = document.lastModified;
+
+// Insert last modified into the second footer paragraph
+document.getElementById("last-modified").textContent = lastModified;
