63c3d4f32f1a672a33084138e0954bc1bcfb5026
Nanyangu-ommy <onanseta@byupathway.edu>
2025-09-18 07:59:09 -1000

    updated temple Album

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -1,12 +1,12 @@
+lastModified.js
 
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-document.getElementById("lastmodified").textContent = document.lastModified;
+// Get the current year
+const currentYear = new Date().getFullYear();
+// Insert current year into the first footer paragraph
+document.querySelector("footer p").innerHTML = `&copy; ${currentYear} 🌸 Omega Ommy Nanseta 🌸 Malawi`;
+// Get last modified date
+const lastModified = document.lastModified;
+// Insert last modified into the second footer paragraph
+document.getElementById("last-modified").textContent = lastModified;
 
-const menuButton = document.querySelector("#menu");
-const navigation = document.querySelector(".navigation");
-
-menuButton.addEventListener("click", () => {
-  navigation.classList.toggle("open");
-  menuButton.textContent = navigation.classList.contains("open") ? "X" : "☰";
-});
 
