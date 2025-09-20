e548445dcf097a4f9c5a685c3a37bee804f45782
Nanyangu-ommy <onanseta@byupathway.edu>
2025-09-19 18:06:28 -1000

    Temple Album updated

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,20 +1,10 @@
-document.getElementById("year").textContent = new Date().getFullYear();
-
-document.getElementById("lastModified").textContent = document.lastModified;
-
-
-
-const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("navMenu");
-
-if (hamburger && navMenu) {
-    hamburger.addEventListener("click", () => {
-        navMenu.classList.toggle("show");
-
-        if (hamburger.textContent === "☰") {
-            hamburger.textContent = "✖";
-        } else {
-            hamburger.textContent = "☰";
-        }
-    });
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const yearSpan = document.getElementById("year");
+    if (yearSpan) {
+        yearSpan.textContent = new Date().getFullYear();
+    }
+    const lastModifiedSpan = document.getElementById("lastmodified");
+    if (lastModifiedSpan) {
+        lastModifiedSpan.textContent = document.lastModified;
+    }
+});
